cbb40408fc0e4d71d5aa43690c6afd1ca543f8a0
belhadj haitam <114673480+H-belhadj@users.noreply.github.com>
2023-06-08 16:41:07 +0100

    Add files via upload

--- so_long.c
+++ so_long.c
@@ -1,19 +1,140 @@
-// /* ************************************************************************** */
-// /*                                                                            */
-// /*                                                        :::      ::::::::   */
-// /*   so_long.c                                          :+:      :+:    :+:   */
-// /*                                                    +:+ +:+         +:+     */
-// /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
-// /*                                                +#+#+#+#+#+   +#+           */
-// /*   Created: 2023/05/14 16:46:14 by hbelhadj          #+#    #+#             */
-// /*   Updated: 2023/05/15 17:39:55 by hbelhadj         ###   ########.fr       */
-// /*                                                                            */
-// /* ************************************************************************** */
+#include "so_long.h"
 
- #include "so_long.h"
+typedef struct  s_list{
+    int position[2];  // Player position [X, Y]
+    char **map;  // Map data
+    int moves; // how many times the player moved
+    void    *wll;
+    void    *backgound;
+    void    *door; 
+    void    *player;
+    void    *coins;
+    void *win;
+    void *mlx;
+    int coin_count; // Number of coins remaining
+} t_list;
+
+
+int update(t_list *so_long)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (so_long->map[i]) {
+        j = 0;
+        while (so_long->map[i][j]) {
+            if (so_long->map[i][j] == '0')
+                mlx_put_image_to_window(so_long->mlx, so_long->win, so_long->backgound, j * 250, i * 250);
+            if (so_long->map[i][j] == '1')
+                mlx_put_image_to_window(so_long->mlx, so_long->win, so_long->wll, j * 250, i * 250);
+            if (so_long->map[i][j] == 'C')
+                mlx_put_image_to_window(so_long->mlx, so_long->win, so_long->coins, j * 250, i * 250);
+            if (so_long->map[i][j] == 'E')
+            {
+                mlx_put_image_to_window(so_long->mlx, so_long->win, so_long->backgound, j * 250, i * 250);
+                mlx_put_image_to_window(so_long->mlx, so_long->win, so_long->door, j * 250, i * 250);
+
+            }
+            if (so_long->map[i][j] == 'P')
+                mlx_put_image_to_window(so_long->mlx, so_long->win, so_long->backgound, j * 250, i * 250);
+            j++;
+        }
+        i++;
+    }
+    mlx_put_image_to_window(so_long->mlx, so_long->win, so_long->player, so_long->position[X] , so_long->position[Y] );
+
+    printf("movement: %d\n", so_long->moves);
+    if ( so_long->coin_count == 0 && so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250] == 'E') {
+
+        exit(0);
+    }
+    return 0;
+}
+int hook(int keycode, t_list *so_long)
+{
+    if (keycode == 53)
+        exit(0);
+    if (keycode == 124) // Right arrow key
+    {
+        // Check if the player is moving to a valid position
+        if (so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250 + 1] != '1') {
+            // if (so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250 + 1] != 'E')
+            //     so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250] = '0';
+
+            so_long->position[X] += 250;
+
+            // so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250] = 'P';
+
+            so_long->moves++;
+        }
+    }
+    if (keycode == 123) // Left arrow key
+    {
+        // Check if the player is moving to a valid position
+        if (so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250 - 1] != '1') {
+
+            // if (so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250 + 1] != 'E')
+            //     so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250] = '0';
+
+            so_long->position[X] -= 250;
+
+            // so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250] = 'P';
+
+            so_long->moves++;
+        }
+    }
+    if (keycode == 125) // Down arrow key
+    {
+        // Check if the player is moving to a valid position
+        if (so_long->map[so_long->position[Y] / 250 + 1][so_long->position[X] / 250] != '1') {
+
+            // if (so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250 + 1] != 'E')
+            //     so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250] = '0';
+
+            so_long->position[Y] += 250;
+
+            // so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250] = 'P';
+
+            so_long->moves++;
+        }
+    }
+    if (keycode == 126) // Up arrow key
+    {
+        // Check if the player is moving to a valid position
+        if (so_long->map[so_long->position[Y] / 250 - 1][so_long->position[X] / 250] != '1') {
+
+            // if (so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250 + 1] != 'E')
+            //     so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250] = '0';
+
+            so_long->position[Y] -= 250;
+
+            // so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250] = 'P';
+
+            so_long->moves++;
+        }
+    }
+
+    // Check if the player collected a coin
+    if (so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250] == 'C') {
+        so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250] = '0';
+        so_long->coin_count--;
+    }
+
+    // Check if the game is over (all coins collected and player at the exit)
+    if ( so_long->coin_count ==0 && so_long->map[so_long->position[Y] / 250][so_long->position[X] / 250] == 'E') {
+        printf("Congratulations! You collected all the coins and reached the exit!\n");
+        exit(0);
+    }
+
+    return 1;
+}
+
+
 
 int main(int argc, char **argv)
 {
+    t_list *so_long = malloc(sizeof(t_list));
     char **stored;
     char *extension;
     char *map_file;
@@ -24,136 +145,116 @@ int main(int argc, char **argv)
     int cnt;
     int i; 
     int j;
-    int exit;
-    int collec;
-    int start;
+    int exit_count;
+    int collec_count;
+    int start_count;
     int row_length;
-
-    
-    if(argc != 2)
-    {
-        printf("ERROR : Invalid number of arguments\n");
+    if (argc != 2) {
+        printf("ERROR: Invalid number of arguments\n");
         return (1);
     }
 
     extension = ".ber";
     map_file = argv[1];
-    if(ft_strlen(map_file) < ft_strlen(extension) || strcmp(map_file + ft_strlen(map_file) - ft_strlen(extension), extension) != 0)
-    {
-        printf("ERROR : map filename must have .ber extenstion\n");
+    if (ft_strlen(map_file) < ft_strlen(extension) || strcmp(map_file + ft_strlen(map_file) - ft_strlen(extension), extension) != 0) {
+        printf("ERROR: Map filename must have .ber extension\n");
         return (1);
     }
 
     fd = open(argv[1], O_RDONLY);
     cnt = 1;
     total = NULL;
-    while(1)
-    {
+    while (1) {
         str = get_next_line(fd);
-        if(!str)
+        if (!str)
             break;
-        total = ft_strjoin(total,str);
+        total = ft_strjoin(total, str);
         cnt++;
     }
     close(fd);
-    
-    stored = ft_split(total,'\n');
+    so_long->map = ft_split(total, '\n');
+    so_long->moves = 0;
 
     i = 0;
-    exit = 0;
-    collec = 0;
-    start = 0;
-    row_length = ft_strlen(stored[0]);
-    while(stored[i])
-    {
+    exit_count = 0;
+    start_count = 0;
+    int player_pos[2];
+
+    row_length = ft_strlen(so_long->map[0]);
+    while (so_long->map[i]) {
         j = 0;
-        while(stored[i][j])
-        {
+        while (so_long->map[i][j]) {
             allowed_char = "01CEP";
-            if(!strchr(allowed_char, stored[i][j]))
-            {
-                printf("WARNING : The map can be composed of only these 5 characters: 0, 1, C, E, P\n");
+            if (!strchr(allowed_char, so_long->map[i][j])) {
+                printf("WARNING: The map can only contain these 5 characters: 0, 1, C, E, P\n");
                 return (1);
             }
-            if(stored[i][j] == 'E')
-                exit++;
-            else if(stored[i][j] == 'C')
-                collec++;
-            else if (stored[i][j] == 'P')
-                start++; 
+            if (so_long->map[i][j] == 'E')
+                exit_count++;
+            else if (so_long->map[i][j] == 'C')
+                collec_count++;
+            else if (so_long->map[i][j] == 'P') {
+                player_pos[X] = j * 250;
+                player_pos[Y] = i * 250;
+                start_count++;
+            }
             j++;
         }
-        if(ft_strlen(stored[i]) != row_length)
-        {
-            printf("ERROR : The map is not rectangular.\n");
+        if (ft_strlen(so_long->map[i]) != row_length) {
+            printf("ERROR: The map is not rectangular.\n");
             return (1);
+        }
+        i++;
+    }
+    so_long->coin_count = collec_count;
 
-        }
-        i++;
-    }
     i = 0;
-    while (stored[0][i] != '\0')
-    {
-        if(stored[0][i] != '1' || stored[cnt - 2][i] != '1')
-        {
-            printf("ERROR : The map must be surrounded by walls.\n");
+    while (so_long->map[0][i] != '\0') {
+        if (so_long->map[0][i] != '1' || so_long->map[cnt - 2][i] != '1') {
+            printf("ERROR: The map must be surrounded by walls.\n");
             return (1);
         }
         i++;
     }
+
     i = 0;
-    while(stored[i] != NULL)
-    {
-        if(stored[i][0] != '1' || stored[i][row_length - 1] != '1')
-        {
-            printf("ERROR : The map must be surrounded by walls.\n");
+    while (so_long->map[i] != NULL) {
+        if (so_long->map[i][0] != '1' || so_long->map[i][row_length - 1] != '1') {
+            printf("ERROR: The map must be surrounded by walls.\n");
             return (1);
         }
         i++;
     }
     
-    if (exit != 1 || collec < 1 || start != 1)
-    {
-        if(exit > 1 || start > 1)
-        {
-            printf("ERROR : The map must not contain duplicates of characters (exit/start).\n");
+    if (exit_count != 1 || collec_count < 1 || start_count != 1) {
+        if (exit_count > 1 || start_count > 1) {
+            printf("ERROR: The map must not contain duplicates of characters (exit/start).\n");
             return (1);
         }
-        printf("ERROR : The map must contain 1 exit, at least 1 collectible, and 1 starting position to be valid");
+        printf("ERROR: The map must contain 1 exit, at least 1 collectible, and 1 starting position to be valid.\n");
         return (1);
     }
+
     printf("YOUR MAP IS GOOD\n");
-    void    *win;
+
     int high = 50;
-    int  width = 50;
-    void    *mlx = mlx_init();
-    printf("%d\n", cnt);
-    printf("%d\n", ft_strlen(stored[0]));
-    win = mlx_new_window(mlx, ft_strlen(stored[0]) * 250,cnt * 250, "tangawi");
-    void    *wll = mlx_xpm_file_to_image(mlx, "textures/wall.xpm", &high, &width);
-    void    *door = mlx_xpm_file_to_image(mlx, "textures/door.xpm", &high, &width);
-    void    *player = mlx_xpm_file_to_image(mlx, "textures/player.xpm", &high, &width);
-    void    *coins = mlx_xpm_file_to_image(mlx, "textures/coin.xpm", &high, &width);
+    int width = 50;
+    so_long->mlx = mlx_init();
+    so_long->win = mlx_new_window(so_long->mlx, ft_strlen(so_long->map[0]) * 250, (cnt-1) * 250, "tangawi w rassi 3ali so_long");
+    so_long->wll = mlx_xpm_file_to_image(so_long->mlx, "textures/wall.xpm", &high, &width);
+    so_long->door = mlx_xpm_file_to_image(so_long->mlx, "textures/door.xpm", &high, &width);
+    so_long->player = mlx_xpm_file_to_image(so_long->mlx, "textures/player.xpm", &high, &width);
+    so_long->coins = mlx_xpm_file_to_image(so_long->mlx, "textures/coin.xpm", &high, &width);
+    so_long->backgound = mlx_xpm_file_to_image(so_long->mlx, "textures/backgound.xpm", &high, &width);
+    
 
+    so_long->position[X] = player_pos[X];
+    so_long->position[Y] = player_pos[Y];
 
-    i= 0;
-    while(stored[i])
-    {
-        j = 0;
-        while(stored[i][j])
-        {
-            if(stored[i][j] == '1')
-                mlx_put_image_to_window(mlx, win, wll, j * 250 ,i * 250 );
-            if(stored[i][j] == 'C')
-                mlx_put_image_to_window(mlx, win, coins,j * 250 ,i * 250);
-            if(stored[i][j] == 'E')
-                mlx_put_image_to_window(mlx, win, door, j * 250 ,i * 250);
-            if(stored[i][j] == 'P')
-                mlx_put_image_to_window(mlx, win, player, j * 250 ,i * 250);
-            j++;
-        }
-        i++;
-    }
-    mlx_loop(mlx);
-    free(stored);
+    printf("ok\n");
+    mlx_hook(so_long->win, ON_KEYDOWN, 1 << 0, hook, so_long);
+    mlx_loop_hook(so_long->mlx,update,so_long);
+    mlx_loop(so_long->mlx);
+    // free(so_long->map);
+    // free(so_long);
 }
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 20:10:04 by hbelhadj          #+#    #+#             */
-/*   Updated: 2023/05/17 22:31:18 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2023/06/07 13:01:18 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,19 @@ char            *ft_substr(char  *s, unsigned int start, int len);
 int             ft_count_world(char *s, char c);
 char	        **ft_split(char *s, char c);
 
+enum {
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+};
+
+enum {
+	X,
+    Y
+};
 
 #endif
